80ef5140f6a08820326c1eccbd73c862c4a6a2a0
Tutku Ince <tutku.ince@outlook.com>
2024-09-18 23:01:52 +0300

    Build Reactive Add Employee REST API

---
+++ src/main/java/com/incetutku/springbootwebflux/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.incetutku.springbootwebflux.controller;
+
+import com.incetutku.springbootwebflux.dto.EmployeeDto;
+import com.incetutku.springbootwebflux.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto) {
+        return employeeService.saveEmployee(employeeDto);
+    }
+}
