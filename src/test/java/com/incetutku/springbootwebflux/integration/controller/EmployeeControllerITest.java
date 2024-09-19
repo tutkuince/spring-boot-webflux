6576ab54b33c6dc0289d3359b2c3cf558277e85c
Tutku Ince <tutku.ince@outlook.com>
2024-09-19 23:07:57 +0300

    Refactored

--- src/test/java/com/incetutku/springbootwebflux/integration/controller/EmployeeControllerITest.java
+++ src/test/java/com/incetutku/springbootwebflux/integration/controller/EmployeeControllerITest.java
@@ -3,6 +3,7 @@ package com.incetutku.springbootwebflux.integration.controller;
 import com.incetutku.springbootwebflux.dto.EmployeeDto;
 import com.incetutku.springbootwebflux.integration.AbstractContainerBaseTest;
 import com.incetutku.springbootwebflux.service.EmployeeService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +21,19 @@ public class EmployeeControllerITest extends AbstractContainerBaseTest {
     @Autowired
     private WebTestClient webTestClient;
 
-    @DisplayName("Integration test for Save Employee")
-    @Test
-    void testSaveEmployee() {
-        EmployeeDto employeeDto = new EmployeeDto();
+    private EmployeeDto employeeDto;
+
+    @BeforeEach
+    void setUp() {
+        employeeDto = new EmployeeDto();
         employeeDto.setName("Tutku");
         employeeDto.setSurname("Ince");
         employeeDto.setEmail("ti@mail.com");
+    }
+
+    @DisplayName("Integration test for Save Employee")
+    @Test
+    void testSaveEmployee() {
 
         WebTestClient.ResponseSpec response = webTestClient.post().uri("/api/v1/employees")
                 .contentType(MediaType.APPLICATION_JSON)
